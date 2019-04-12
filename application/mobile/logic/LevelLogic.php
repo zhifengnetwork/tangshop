d4bc50b45548a66aa59ef5ea56fd57c338e80690
BlizzardWu <2442668602@qq.com>
2019-04-12 15:14:36 +0800

    修改折扣 购物车价格

--- application/mobile/logic/LevelLogic.php
+++ application/mobile/logic/LevelLogic.php
@@ -26,14 +26,17 @@ class LevelLogic
             if($nums >= 3){
                 Db::name('users')->where('user_id',$user_id)->setInc('level');
                 Db::name('user_level_time')->data(['user_id'=>$user_id,'user_level'=>$user_level+1,'level_time'=>$time])->insert();
+                $this->change_discount($user_id);
             }
         }else{
-            // //如果用户等级等于最大等级
+            //如果该等级已买的指定商品数大于升级需要的
             while($nums['goods_num'] >= $nums['need_num']){
+                //如果用户等级等于最大等级
                 if($user_level != $max_level){
                     Db::name('users')->where('user_id',$user_id)->setInc('level');
                     $user_level = M('users')->where('user_id',$user_id)->value('level');
                     Db::name('user_level_time')->data(['user_id'=>$user_id,'user_level'=>$user_level,'level_time'=>$time])->insert();
+                    $this->change_discount($user_id);
                     $nums = $this->condition($user_id);
                     if(empty($nums['need_num'])){
                         break;
@@ -103,6 +106,14 @@ class LevelLogic
             ];
             return $nums;
         }
+    }
+
+    //改变users表里的对应等级折扣
+    public function change_discount($user_id)
+    {
+        $level = M('users')->where('user_id',$user_id)->value('level');
+        $discount = M('user_level')->where('level_id',$level)->value('discount');
+        M('users')->where('user_id',$user_id)->update(['discount'=>$discount/100]);
     }
 
 
