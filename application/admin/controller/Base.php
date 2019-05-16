fc1b53895362e085b7cae945616aba3f992d0320
BlizzardWu <2442668602@qq.com>
2019-05-16 15:41:50 +0800

    ttt

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -15,6 +15,7 @@
  */
 
 namespace app\admin\controller;
+use app\admin\logic\UpgradeLogic;
 use app\common\logic\Saas;
 use think\Controller;
 use think\Db;
@@ -35,6 +36,9 @@ class Base extends Controller {
         Session::start();
         header("Cache-control: private");  // history.back返回后输入框值丢失问题 参考文章 http://www.tp-shop.cn/article_id_1465.html  http://blog.csdn.net/qinchaoguang123456/article/details/29852881
         parent::__construct();
+        $upgradeLogic = new UpgradeLogic();
+        $upgradeMsg = $upgradeLogic->checkVersion(); //升级包消息
+        $this->assign('upgradeMsg',$upgradeMsg);
         //用户中心面包屑导航
         $navigate_admin = navigate_admin();
         $this->assign('navigate_admin',$navigate_admin);
